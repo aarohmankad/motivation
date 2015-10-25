3df91f810fb801380fd6b56b82e6c72787a2cc05
Aaroh <aarohmankad@gmail.com>
2015-10-24 22:45:50 -0700

    better sizes for fonts

---
+++ js/app.js
@@ -0,0 +1,47 @@
+(function(){
+
+var $  = document.getElementById.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+
+var App = function($el){
+  this.$el = $el;
+  this.load();
+};
+
+App.fn = App.prototype;
+
+App.fn.load = function(){
+  this.renderAgeLoop();
+};
+
+App.fn.renderAgeLoop = function(){
+  this.interval = setInterval(this.renderAge.bind(this), 100);
+};
+
+App.fn.renderAge = function(timeLeft){
+  var now = new Date(Date.now());
+  var timeLeft = countdown(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 24)).toString();
+
+  requestAnimationFrame(function(){
+    this.html(this.view('timeLeft')({
+      timeLeft: timeLeft,
+    }));
+  }.bind(this));
+};
+
+App.fn.$$ = function(sel){
+  return this.$el.querySelectorAll(sel);
+};
+
+App.fn.html = function(html){
+  this.$el.innerHTML = html;
+};
+
+App.fn.view = function(name){
+  var $el = $(name + '-template');
+  return Handlebars.compile($el.innerHTML);
+};
+
+window.app = new App($('app'))
+
+})();
